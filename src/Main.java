9f9b01689f5695751f4417f0d67f3eb0035acc5d
Name Surname <vladislavabasarina8@gmail.com>
2023-11-22 12:34:03 +0300

    change commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int bilet = 3500;
+        int cena_za_odny_mily = 20;
+        int bonusi = bilet/cena_za_odny_mily;
+
+            System.out.println("Итоговый бонус = " + bonusi);
+        }
+    }
